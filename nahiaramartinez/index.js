109bcea20d30e17e9f39f6b1f2ae2a0989f51258
Lucas Dominguez <lucasdomin62@gmail.com>
2023-08-15 16:54:21 -0300

    Actividades JS

---
+++ nahiaramartinez/index.js
@@ -0,0 +1,33 @@
+const numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+numbers[2] = 30;
+console.log(numbers);
+
+/* [10, 2, 30, 4, 5] lo que pasa con esto esque lo que yo pondo adentro del corchete el numero que 
+yo quiero cambiar*/
+
+let numero = [1, 2, 3, 4];
+
+function sumarPares(numeros) {
+  let suma = 0;
+  let i = 0; // lo usaremos como índice
+  while (i < numeros.length) {
+    if (numeros[i] % 2 === 0) {
+      suma = suma + numeros[i];
+    }
+    console.log(suma); // imprime el elemento en la posición i
+    i++; // incrementamos el índice en 1 para la siguiente iteración
+  }
+}
+sumarPares(numero);
+
+//Ejercicio:
+//Crear una función que reciba un array de palabras y devuelva true si todas las palabras terminan con la letra "a" y false si al menos una palabra no termina con la letra "a".
+
+let palabras = ["juega", "casa", "mesa"];
+function terminanEnA(words) {
+  const todosTerminanEnA = words.every((word) => word.endsWith("a"));
+  console.log(todosTerminanEnA);
+}
+
+terminanEnA(palabras);
