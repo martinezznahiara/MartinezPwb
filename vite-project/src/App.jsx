c10555649c62915d57f3eaedf21ad4624ece539a
nahiara martinez <martineznahiara311@gmail.com>
2023-09-12 17:03:25 -0300

    react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      hola mundo{" "}
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+export const Header = () => {
+  return (
+    <>
+      <div className="name">
+        <h1>titulo</h1>
+        <nav>
+          <a> link 1 </a>
+          <a>link 2</a>
+        </nav>
+      </div>
+    </>
+  );
+};
